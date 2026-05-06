8e1f9419c27a861b95223150675b48edb1f183a3
Muniox <muniox20@gmail.com>
2026-05-06 06:16:39 +0200

    Add ClaimsPrincipal extensions and UserInfoDto to support Keycloak user info mapping

---
+++ Keycloak/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,21 @@
+using System.Security.Claims;
+
+namespace Keycloak;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static UserInfoDto ToUserInfoDto(this ClaimsPrincipal user)
+    {
+        var identity = (ClaimsIdentity)user.Identity!;
+
+        return new UserInfoDto(
+            UserId: Guid.Parse(user.FindFirstValue("sub")!),
+            Email: user.FindFirstValue("email") ?? "",
+            EmailVerified: bool.TryParse(user.FindFirstValue("email_verified"), out var v) && v,
+            Username: user.FindFirstValue("preferred_username") ?? "",
+            DisplayName: user.FindFirstValue("name") ?? "",
+            GivenName: user.FindFirstValue("given_name") ?? "",
+            FamilyName: user.FindFirstValue("family_name") ?? "",
+            Roles: user.FindAll(identity.RoleClaimType).Select(c => c.Value).ToArray());
+    }
+}
---
+++ Keycloak/UserInfoDto.cs
@@ -0,0 +1,11 @@
+namespace Keycloak;
+
+public sealed record UserInfoDto(
+    Guid UserId,
+    string Email,
+    bool EmailVerified,
+    string Username,
+    string DisplayName,
+    string GivenName,
+    string FamilyName,
+    IReadOnlyCollection<string> Roles);
